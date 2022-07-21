4b80f74d59960524ae222b0ab8fd620ec549d47a
kneelanda <108554852+kneelanda@users.noreply.github.com>
2022-07-21 16:27:37 -0700

    Simple (#1)
    * Update game.py
    
    * new update

--- game.py
+++ game.py
@@ -8,9 +8,14 @@ from random import choice
 # USER SELECTION
 #
 
+#removing duplication?
+#goal is so that in the future we only need to update some concept in one place
+#so we aren't able to forget to update all the places 
+valid_choices = ["rock", "paper", "scissors"]
+
 u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
 print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
+if u not in valid_choices:
     print("OOPS, TRY AGAIN")
     exit()
 
@@ -18,7 +23,7 @@ if u not in ["rock", "paper", "scissors"]:
 # COMPUTER SELECTION
 #
 
-c = choice(["rock", "paper", "scissors"])
+c = choice(valid_choices)
 print("COMPUTER CHOICE:", c)
 
 #
