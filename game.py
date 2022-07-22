89ee7e944759c58ab838595b969f2d63b393ada4
kneelanda <ashlin.jb.kneeland@gmail.com>
2022-07-21 17:39:55 -0700

    bug fix

--- game.py
+++ game.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
     print("USER CHOICE:", u)
     if u not in valid_choices:
-        print("OOPS, TRY AGAIN")
+       return "OOPS, TRY AGAIN"
         exit()
 
     #
@@ -32,29 +32,30 @@ if __name__ == "__main__":
     #
 
     c = choice(valid_choices)
-    print("COMPUTER CHOICE:", c)
+    return "COMPUTER CHOICE:"
 
     #
     # DETERMINATION OF WINNER
     #
 
-    if u == "rock" and c == "rock":
-        print("It's a tie!")
-    elif u == "rock" and c == "paper":
-        print("The computer wins")
-    elif u == "rock" and c == "scissors":
-        print("The user wins")
+    if user_choice == "rock" and computer_choice == "rock":
+        return "It's a tie!"
+    elif user_choice == "rock" and computer_choice == "paper":
+        return "The computer wins"
+    elif user_choice == "rock" and computer_choice == "scissors":
+        return "The user wins"
 
-    elif u == "paper" and c == "rock":
-        print("The computer wins")
-    elif u == "paper" and c == "paper":
-        print("It's a tie!")
-    elif u == "paper" and c == "scissors":
-        print("The user wins")
+    elif user_choice == "paper" and computer_choice == "rock":
+        return "The computer wins"
+    elif user_choice == "paper" and computer_choice == "paper":
+        return "It's a tie!"
+    elif user_choice == "paper" and computer_choice == "scissors":
+        #return "The user wins" bug 
+         return "The computer wins"
 
-    elif u == "scissors" and c == "rock":
-        print("The computer wins")
-    elif u == "scissors" and c == "paper":
-        print("The user wins")
-    elif u == "scissors" and c == "scissors":
-        print("It's a tie!")
+    elif user_choice == "scissors" and computer_choice == "rock":
+        return "The computer wins"
+    elif user_choice == "scissors" and computer_choice == "paper":
+        return "The user wins"
+    elif user_choice == "scissors" and computer_choice == "scissors":
+        return "It's a tie!"
